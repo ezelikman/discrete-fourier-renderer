5649af5c1ddabae8fa701be9905f110652ecea42
Eric Zelikman <eric.zelikman@r6zelikmaneric.rih.org>
2014-10-02 08:11:54 -0400

    Initial Commit

---
+++ FourierRenderer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FourierRenderer
+//
+//  Created by Eric Zelikman on 10/2/14.
+//  Copyright (c) 2014 Eric Zelikman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
